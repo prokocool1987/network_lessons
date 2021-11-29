98aba55a29696dfa8daf96a3e9062a4eee983499
Максим Нагорный <prokocool@mail.ru>
2021-11-29 12:31:20 +1000

    Задача 6.1

--- exercises/09_functions/task_9_1.py
+++ exercises/09_functions/task_9_1.py
@@ -75,3 +75,17 @@ def generate_access_config(intf_vlan_mapping, access_template):
 
     Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
     """
+    commands = []
+    for key, value in intf_vlan_mapping.items():
+
+        commands.append("interface " + key)
+        for command in access_template:
+            if command.endswith("access vlan"):
+                commands.append(command + " " + str(value))
+            else:
+                commands.append(command)
+
+    return commands
+
+#print(generate_access_config(access_config_2, access_mode_template))
+
